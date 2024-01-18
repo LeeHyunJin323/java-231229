b47b70079b6afaf9fd1fa58bc65de44857868bbb
LeeHyunJin323 <skrk3042@naver.com>
2024-01-18 11:51:28 +0900

    20240118

--- JAVA_bt1228/src/j_Chapter09/Z_Practice01.java
+++ JAVA_bt1228/src/j_Chapter09/Z_Practice01.java
@@ -12,45 +12,48 @@ package j_Chapter09;
 // : checkAnimalType 메서드 - Animal 타입의 매개변수를 전달
 // : instanceof 연산자를 사용하여 매개변수가 Cat과 Dog 중 어느 인스턴스인지 확인
 // : Cat > 'cat' 출력 /Dog > 'dog' 출력 / 둘 다 X > 'Unknown animal type'
-class Animal1{
-	void sound() {
-		System.out.println("The animal makes a sound");
-	}
-	
+class Animal1 {
+    void sound() {
+        System.out.println("The animal makes a sound");
+    }
 }
-class Cat extends Animal1{
-	void sound() {
-		System.out.println("meow");
-	}
+
+class Cat extends Animal1 {
+    void sound() {
+        System.out.println("meow");
+    }
 }
-class Dog1 extends Animal1{
-	void sound() {
-		System.out.println("The animal makes a sound");
-	}
+
+class Dog1 extends Animal1 {
+    void sound() {
+        System.out.println("bark");
+    }
 }
+
+
+class AnimalChecker {
+    static void checkAnimalType(Animal1 animal) {
+        if (animal instanceof Dog1) {
+            System.out.println("Dog > 'dog'");
+        } else if (animal instanceof Cat) {
+            System.out.println("Cat > 'cat'");
+        } else {
+            System.out.println("Unknown animal type");
+        }
+    }
+}
+
 public class Z_Practice01 {
+    public static void main(String[] args) {
+        // Animal, Cat, Dog 클래스의 객체를 Animal 타입으로 저장
+        // 생성된 각 객체를 checkAnimalType 메서드를 사용하여 확인
+        Animal1 myanimal = new Animal1();
+        Animal1 mydog = new Dog1();
+        Animal1 mycat = new Cat();
 
-	public static void main(String[] args) {
-		
-		//Animal , Cat , Dog 클래스의 객체를 Animal타입으로 저장
-		// 생성된 각 객체를 checkAnimalType 메서드를 사용하여 확인
-		Animal1 myanimal = new Animal1();
-		Animal1 mydog = new Dog1();
-		Animal1 mycat = new Cat();
-		if (mydog instanceof Animal1) {
-			System.out.println("Dog > 'dog''");
-		}
-		
-		if (mycat instanceof Animal1) {
-			System.out.println("Cat > 'cat'");
-		}
-		
-		if (Animal1 instanceof Car) {
-			System.out.println("운송수단은 차입니다.");
-		} else 
-			System.out.println("운송수단은 차가 아닙니다.");
-		
-
-	}
-
+        AnimalChecker.checkAnimalType(myanimal);
+        AnimalChecker.checkAnimalType(mydog);
+        AnimalChecker.checkAnimalType(mycat);
+    }
 }
+
